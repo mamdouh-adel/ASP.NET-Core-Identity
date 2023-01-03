f52cdb533f23e7ec146c896c9949c0f85f8ecee3
Mamdouh Adel <mamdouh.elmallah@yahoo.com>
2023-01-03 14:05:06 +0200

    Unit 3 done

---
+++ RazorPagesPizza/Migrations/20230103114923_CreateIdentitySchema2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesPizza.Migrations
+{
+    public partial class CreateIdentitySchema2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
